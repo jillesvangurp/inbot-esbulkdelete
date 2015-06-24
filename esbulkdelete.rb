c95691cc9cc81d7a13ddbc9532b72fb3718dfc1a
Jilles van Gurp <jilles@jillesvangurp.com>
2015-06-24 12:56:00 +0200

    support command line args

--- esbulkdelete.rb
+++ esbulkdelete.rb
@@ -1,12 +1,14 @@
 #!/usr/bin/env ruby
 require 'elasticsearch'
+require 'json'
+require 'optparse'
 
-def bulkdelete(host, query, page_size)
+def bulkdelete(host, query, page_size, index, type)
   client = Elasticsearch::Client.new log: false, host: host
 
   scrolltime='10m'
 
-  results = client.search search_type: 'scan', scroll: scrolltime, size: page_size, body: query, fields: []
+  results = client.search search_type: 'scan', scroll: scrolltime, size: page_size, body: query, fields: [], index: index, type: type
 
   while results = client.scroll(scroll_id: results['_scroll_id'], scroll: scrolltime) and not results['hits']['hits'].empty? do
     bulk_items=[]
@@ -18,14 +20,29 @@ def bulkdelete(host, query, page_size)
 end
 
 
-query={
-  query: {
-    filtered: {
-      filter: {
-        match_all: {}
-      }
-    }
-  }
+#bulkdelete(host: 'localhost:9200',query: query, page_size: 1000)
+
+options = {
+  host: 'localhost:9200',
+  index: '_all',
+  type: '_all'
 }
 
-bulkdelete(host: 'localhost:9200',query: query, page_size: 1000)
+OptionParser.new do |opts|
+  opts.banner = "Usage: example.rb [options]"
+
+  opts.on("-h", "--host [HOST]", "Run verbosely") do | h |
+    options[:host] = h
+  end
+  opts.on("-q", "--query QUERY", "Run verbosely") do | q |
+    options[:query] = JSON.parse q
+  end
+end.parse!
+
+
+if options[:query]
+  puts options
+  bulkdelete(options[:host], options[:query], 1000, options[:index], options[:type])
+else
+  puts "specify valid json query with -q"
+end
